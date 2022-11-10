c8bd4470d0aa1a5c92b84a7febd30e5cf1602bc3
Sergio <contact@sealguzmanal@ittepic.edu.mx>
2022-11-09 23:19:41 -0700

    LienzoTangram inicio

---
+++ app/src/main/java/mx/edu/ittepic/tangram_18401126_18401179_18401186/ui/LienzoTangram.kt
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.tangram_18401126_18401179_18401186.ui
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.View
+import mx.edu.ittepic.tangram_18401126_18401179_18401186.MainActivity
+
+class LienzoTangram (p:MainActivity): View(p){
+    override fun onDraw(c: Canvas) {
+        val p = Paint()
+        super.onDraw(c)
+    }
+}
